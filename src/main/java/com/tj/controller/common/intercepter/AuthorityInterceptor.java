513c45070a5b9065ae1364209d53ac650b8bf2c1
Jin <532688846@qq.com>
2019-01-17 09:43:31 +0800

    v2.0

---
+++ src/main/java/com/tj/controller/common/intercepter/AuthorityInterceptor.java
@@ -0,0 +1,8 @@
+package com.tj.controller.common.intercepter;
+
+public class AuthorityInterceptor {
+
+
+
+
+}
--- src/main/java/com/tj/service/impl/OrderServiceImpl.java
+++ src/main/java/com/tj/service/impl/OrderServiceImpl.java
@@ -778,7 +778,7 @@ public class OrderServiceImpl implements IOrderService {
                 .setUndiscountableAmount(undiscountableAmount).setSellerId(sellerId).setBody(body)
                 .setOperatorId(operatorId).setStoreId(storeId).setExtendParams(extendParams)
                 .setTimeoutExpress(timeoutExpress)
-                .setNotifyUrl("http://39.98.94.211/shopping/order/alipay_callback.do")//支付宝服务器主动通知商户服务器里指定的页面http路径,根据需要设置
+                .setNotifyUrl("http://39.98.94.211:8080/shopping/order/alipay_callback.do")//支付宝服务器主动通知商户服务器里指定的页面http路径,根据需要设置
                 .setGoodsDetailList(goodsDetailList);
 
         AlipayF2FPrecreateResult result = tradeService.tradePrecreate(builder);
@@ -790,7 +790,9 @@ public class OrderServiceImpl implements IOrderService {
                 dumpResponse(response);
 
                 // 需要修改为运行机器上的路径
-                String filePath = String.format("/usr/tj/ftpTemp/qr-%s.png",
+                /*String filePath = String.format("/usr/tj/ftpTemp/qr-%s.png",
+                        response.getOutTradeNo());*/
+                String filePath = String.format("C:/FTPfile/qr-%s.png",
                         response.getOutTradeNo());
                 log.info("filePath:" + filePath);
                 ZxingUtils.getQRCodeImge(response.getQrCode(), 256, filePath);
